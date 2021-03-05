35fcc80be987c45f6beb7d5f7f35b139be174cdc
Sean Poeschl <sean.poeschl@nutanix.com>
2021-03-05 13:19:55 -0600

    Second Commit

--- HibernateCluster.ps1
+++ HibernateCluster.ps1
@@ -1,9 +1,9 @@
 # Client credentials
 #client_id from the user from the clusters portal
 #User created in clusters portal as Organization Administrator
-$clientId = "c6f42bf6-3496-4c7a-9e90-7302240ee57f.img.frame.nutanix.com"
-$clientSecret = "5f66f8a102c9961f1acbbf521e5447ab14b91be6"
-$cluster_id ="0005BCBD-4522-EE20-754A-90D23F4DFC83" 
+$clientId = "***************************.img.frame.nutanix.com"
+$clientSecret = "***********************************"
+$cluster_id ="***************************************" 
 
 
 $timestamp = [int](Get-Date -UFormat %s)
